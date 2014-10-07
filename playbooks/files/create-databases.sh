46d4685af7cf4eae3ee8b053dc1e45174d7cf337
Julia Kreger <julia.ash.kreger@hp.com>
2014-10-07 14:13:31 -0400

    CORE-1814 Fix database script
    This commit fixes two minor issues with the create-databases.sh
    script that were preventing it from working as intended, as well
    as adds suppression of the database password from being logged
    during this process.
    
    Change-Id: Id35661bd342837d545031bf05e0aa5790688a4e7

--- playbooks/files/create-databases.sh
+++ playbooks/files/create-databases.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-PATH=/usr/local/bin/:$PATH
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -eux
+set -o pipefail
 
+PATH=/usr/local/bin/:$PATH
 # Execute database creation step although suppress output
 # that may contain passwords.
-sed -i 's/| mysql$/| mysql --defaults-file=/mnt/state/root/metadata.my.cnf/' /usr/local/bin/sync-db
+sed -i 's/| mysql$/| mysql --defaults-file=\/mnt\/state\/root\/metadata.my.cnf/' /usr/local/bin/os-db-create
 sync-db -c 2>&1 |grep -v "db_pass" |grep -v "os-db-create"
-sync-db -m
+sync-db -m 2>&1 |grep -v "db_pass" |grep -v "os-db-create"
