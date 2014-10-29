68d17d7a74f6033542b9bb297ac9ccd8108be73a
Cian O'Driscoll <cian.o-driscoll@hp.com>
2014-10-29 11:15:53 +0000

    Call reset-db instead of sync-db
    sync-db change never got merged upstream and was removed from our
    local repo with the latest merge from upstream.
    
    Change-Id: I84363baa0e025bcca794ba4da41a86d78c8dad37

--- playbooks/files/create-databases.sh
+++ playbooks/files/create-databases.sh
@@ -20,5 +20,5 @@ PATH=/usr/local/bin/:$PATH
 # Execute database creation step although suppress output
 # that may contain passwords.
 sed -i 's/| mysql$/| mysql --defaults-file=\/mnt\/state\/root\/metadata.my.cnf/' /usr/local/bin/os-db-create
-sync-db -c 2>&1 |grep -v "db_pass" |grep -v "os-db-create"
-sync-db -m 2>&1 |grep -v "db_pass" |grep -v "os-db-create"
+reset-db -c 2>&1 |grep -v "db_pass" |grep -v "os-db-create"
+reset-db -m 2>&1 |grep -v "db_pass" |grep -v "os-db-create"
