82eb9d86ec8b2931dd5abba25853aa597a2f14a0
Cian O'Driscoll <cian.o-driscoll@hp.com>
2014-11-25 11:23:38 +0000

    Re-activate vg and lvs on rebuild
    Re-creates the loopback devices on rebuild/update.
    Re-enables volume groups and also logical volumes
    
    Change-Id: Ib319eebecfd29c71bc346c48ed47525552d54e94

--- playbooks/files/cleanup_cinder_devices.sh
+++ playbooks/files/cleanup_cinder_devices.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-for CINDER_VOLUME in `dmsetup ls 2>/dev/null | cut -f 1 |grep 'cinder--volumes'|grep -v pool`; do
-   dmsetup remove $CINDER_VOLUME
+for CINDER_VOLUME in `lvdisplay  | grep cinder-volumes | grep 'LV Path' | awk '{print $3}'`; do
+   lvchange -a n $CINDER_VOLUME
 done
 vgchange -a n
